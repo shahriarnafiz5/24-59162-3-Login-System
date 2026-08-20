1467aff4a7cf405729192554435d6f0641ae88bb
Shahriar Nafiz Nahin <arafatshahriar512330@gmail.com>
2026-08-20 21:55:05 +0600

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE DATABASE db_users;
-USE db_users;
-CREATE TABLE tbl_users (
-    id       INT IDENTITY(1,1) PRIMARY KEY,
-    username NVARCHAR(50)  NOT NULL UNIQUE,
-    password NVARCHAR(100) NOT NULL
-);
-INSERT INTO tbl_users (username, password) VALUES ('admin', 'admin123');
-
